f82bd3f04f7ad9cb72c330aae14a97bb26d90b0a
Nagireddy.Vineeth <nagireddysaivineeth@gmail.com>
2026-02-11 23:22:28 +0530

    Introduction to python

---
+++ intro.py
@@ -0,0 +1,40 @@
+#Basic print Statement
+print("Started revising python \n")
+
+#python Indentation
+# We should give minimum 4 spaces
+if 5 > 2:
+    print("five is greater than two \n")
+
+#variables in java
+x = 10
+y = "Hello World"
+
+print(x)
+print(y)
+
+#using multiple statements in a single line
+print("Hello"); print("This is saivineeth \n") 
+
+#printing the statements of different print stmts in the same line
+print("Hello!", end=" ")
+print("My name is sai")
+print()
+
+#Printing numbers
+print(20)
+print(10 + 20)
+print()
+
+#mixinng text with numbers
+print("Currently the time is",1, "PM. \n" )
+
+#comments in python
+print("We use #for the single line comments")
+
+"""This is used
+for Multiliine 
+Comments"""
+
+print("We use triple quotes for multi line comments")
+
