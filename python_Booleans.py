af4f4b958ad64948e1de78c83abe5458d9c3774a
Nagireddy.Vineeth <saivineethnagireddyyt@gmail.com>
2026-02-12 17:34:46 +0530

    Booleans in python

---
+++ python_Booleans.py
@@ -0,0 +1,28 @@
+"""Boolean 
+Which represents the result as either true or false"""
+x = True
+y = False
+print(type(x))
+
+#using logical operators
+a = 10
+b = 5
+
+print(a > b)     # True
+print(a < b)     # False
+print(a == b)    # False
+print(a != b)    # True
+print(a >= b)    # True
+print(a <= b)    # False
+print()
+
+#using if else conditions
+is_logged_in = True
+
+if is_logged_in:
+    print("Welcome!")
+else:
+    print("Please log in")
+
+
+
