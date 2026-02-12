8fde6b5c4982a3628de8f4a9c4b0405fb41aec4f
Nagireddy.Vineeth <saivineethnagireddyyt@gmail.com>
2026-02-12 17:25:51 +0530

    Datatypes in python

---
+++ Dtypes.py
@@ -0,0 +1,15 @@
+#Datatypes in python
+"""There are different types of datatypes in python programming
+for example texttype -str, numeric types - int float and complex, sequencetypes - list tuple range, for mapping - dict, for set type - set and frozenset, Boolean type - bool etc..."""
+
+a = "Hello" #string
+b = 10  #int
+c = 1.5 #float
+d = ["sai", "Vineeth"] #list
+e = ("sai", "Vineeth") #tuple
+f = range(2, 6) #range
+g = {"name": "sai", "age" : 22} #dictionary
+h = frozenset({"sai", "vineeth"})
+i = True
+
+print(a); print(b); print(c); print(d); print(e); print(list(f)); print(g); print(h); print(i); 
