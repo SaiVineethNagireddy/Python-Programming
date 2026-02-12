afbbd08195121c25dcdf0204dfb780b55773efea
Nagireddy.Vineeth <saivineethnagireddyyt@gmail.com>
2026-02-12 17:36:25 +0530

    math functions

---
+++ Python_math.py
@@ -0,0 +1,13 @@
+#Math functions
+import math
+
+num = 16
+
+print("Square root:", math.sqrt(num))
+print("Power:", math.pow(2, 3))
+print("Ceil:", math.ceil(4.3))
+print("Floor:", math.floor(4.7))
+print("Factorial:", math.factorial(5))
+print("Absolute:", math.fabs(-10))
+print("GCD:", math.gcd(12, 18))
+print("Value of pi:", math.pi)
