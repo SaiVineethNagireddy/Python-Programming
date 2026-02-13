bfb752e4e4c2f1135b84de3b30404d206a163107
Nagireddy.Vineeth <saivineethnagireddyyt@gmail.com>
2026-02-13 23:35:21 +0530

    Add OOP example: Classes and Objects

---
+++ OOPS/python_classesandobjects.py
@@ -0,0 +1,26 @@
+#Python Classes/Objects
+
+#Creating a class
+class myname:
+    name = "Saivineeth"
+print(myname)
+print()
+
+#creating an onject to the class
+class myname:
+    name = "Sai"
+x = myname()
+print(x.name)
+print()
+
+#deleting an object
+del x
+#print(x.name)  #shows an error that x is not defined
+print()
+
+#multiple objects
+x = myname()
+y = myname()
+z = myname()
+print(x)
+
